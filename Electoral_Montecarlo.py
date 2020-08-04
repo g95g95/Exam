333d90f1d1d2a1e201fb8860ebe79420b8df8214
Giulio Bonelli <giulio.bonelli@studio.unibo.it>
2020-08-04 13:54:51 +0200

    Input functions completed

--- Electoral_Montecarlo.py
+++ Electoral_Montecarlo.py
@@ -27,6 +27,7 @@ class Montecarlo_electoral:
 		self.checksingpercentage = True
 		self.checktotpercentage  = True
 		self.checkinput          = True
+
 		
 		if self.chooseinput not in set(['stinput','excel','txt']):
 			self.checkinput = False
@@ -38,9 +39,9 @@ class Montecarlo_electoral:
 			
 			self.Npart     = int (input("Enter the number of parties\n"))
 			self.Parties   = [input('Enter the name of the party number '+str(i+1)+'\n') for i in range(self.Npart)]
-			self.Propval   = [float(input('Enter the general results of  '+str(Party)+'\t')) for Party in self.Parties]
-			self.Propcoef  = float(input('Enter the value of the proportional coefficient\n'))
-			self.Majorcoef = float(input('Enter the value of the majority coefficient\n'))
+			self.Propval   = [eval(input('Enter the general results of  '+str(Party)+'\t')) for Party in self.Parties]
+			self.Propcoef  = eval(input('Enter the value of the proportional coefficient\n'))
+			self.Majorcoef = eval(input('Enter the value of the majority coefficient\n'))
 			self.Results   = dict([(self.Parties[i],self.Propval[i]) for i in range(self.Npart)])
 			
 		if self.chooseinput == 'excel':
@@ -50,8 +51,8 @@ class Montecarlo_electoral:
 			
 			self.Parties   = list(xls.columns)
 			self.Propval   = list(xls[Party][0] for Party in self.Parties)
-			self.Propcoeff = list([xls[Party][1] for Party in self.Parties])[:2]
-			self.Majorcoeff= list([xls[Party][2] for Party in self.Parties])[:2]
+			self.Propcoef  = list([xls[Party][1] for Party in self.Parties])[1]
+			self.Majorcoef = list([xls[Party][2] for Party in self.Parties])[1]
 			self.Results   = dict([(self.Parties[i],self.Propval[i]) for i in range(len(self.Parties))])
 		
 		if self.chooseinput == 'txt':
@@ -61,50 +62,95 @@ class Montecarlo_electoral:
 			self.Nparty    = len(file[0].split('\t'))
 			self.Parties   = file[0].split('\t')
 			self.Propval   = file[1].split('\t')
-			self.Propcoef  = float(file[2].split('\t')[1])
-			self.Majorcoef = float(file[3].split('\t')[1])
+			self.Propcoef  = eval(file[2].split('\t')[1])
+			self.Majorcoef = eval(file[3].split('\t')[1])
 			self.Results   = dict([(self.Parties[i],self.Propval[i]) for i in range(self.Nparty)])
 		
 
 				 
 	def check_input(self) :
 			
+		if len(self.Parties) == 0:
+			raise ValueError('No names are inserted')
+		
+		if len(self.Propval) == 0:
+			raise ValueError('No results are inserted')
+			
+		if self.Propcoef == None:
+			raise ValueError('No Proportional coefficient is inserted')
+			
+		if self.Majorcoef  == None:
+			raise ValueError('No major coefficience is inserted')
+		
+		
+		
+		
+		
 		for i in self.Parties:
-			if  math.isnan(i) or not i.islpha():
-				self.checaValue = False
+			
+			if i.isalnum():
+				self.checkinput = True
+			
+			else:
 				raise ValueError('die Name den Parteien du hast eingefügt ist nicht richtig')
 		
 		
 		
 		for i in self.Propval:
-			if math.isnan(i):
-				self.checkValue = False
-				raise ValueError('The value you have inserted is not a number\n')
 			
-			if i.isalpha():
-				self.checkalpha = False
-				raise ValueError('There are letters in your Results!\n')
-			
+			try:
+				i = float(i)
+				
+			except ValueError:
+				self.checkinput = False
+				print('The value you have inserted is not a number\n')
+							
 			if i>1:
-				self.checksingpercentage = False
-				raise ValueError('The value you have inserted is larger than one! Unwirklich!\n')
+				self.checkinput = False
+				raise ValueError('The value you have inserted: ' + str(i)+' is larger than one! Unwirklich!\n')
+			
+			
+			
+		if self.checkinput == True:
+			self.Propval = [float(i) for i in self.Propval]
+			self.Results =  dict([(self.Parties[i],self.Propval[i]) for i in range(len(self.Parties))])
+
+
 			
 		
 				
 		if(sum(self.Propval))>1:
-			self.checktotpercentage = False
-			raise Exception('The sum of the results cannot be larger than one!!! You made a mistake')
+			self.checkinput = False
+			raise ValueError('The sum of the results cannot be larger than one!!! You made a mistake')
 			
 		
+		try:
+			float( self.Majorcoef)
+		except ValueError:
+			self.checkinput = False
+			print('The value of Major coefficient you have inserted is not a number\n')
 	
+		if self.Majorcoef > 1:
+			raise ValueError ('The Major coefficient is larger than one!\n Not possible')
+			
 	
+		try:
+			float( self.Propcoef)
+		except ValueError:
+			self.checkinput = False
+			print('The value of Major coefficient you have inserted is not a number\n')
+	
+		if self.Propcoef > 1:
+			raise ValueError ('The Major coefficient is larger than one!\n Not possible')
 
+		   
+		if self.Propcoef<1 and self.Majorcoef<1 and self.Propcoef + self.Majorcoef >1:
+			raise ValueError('Something is wrong with the values of the two coefficients\n')
 			   
-					   
 				   
 		
-#m = Montecarlo_electoral('excel')
-#m.Import_Results()		
+m = Montecarlo_electoral('txt')
+m.Import_Results()		
 	
     
 
