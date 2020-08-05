8d4e8554af9afb068f7c42382789c63e629584d5
Giulio Bonelli <giulio.bonelli@studio.unibo.it>
2020-08-05 10:45:18 +0200

    First unreviewed sketch of the Theoretical Background and explanation of the program

--- Main_Electoral.py
+++ Main_Electoral.py
@@ -19,35 +19,3 @@ m.Complete_Simulation()
 m.Graphics(Seats2018)
 
 
-
-
-
-
-
-
-
-
-
-
-
-"""The section below plots in an hystogram all the possible results for each party
-in order to point out the possibility of akward results(the tail of distribution
-of a party who got a lesser result than another party overlaps with that other party
-tail's distribution.)
-data = Electoral_Montecarlo.Possible_Results(10000)
-plt.hist(data['Movimento 5 stelle'],bins,label = 'M5S')
-plt.hist(data['Centrodestra'], bins, label='Cdx')
-plt.hist(data['Centrosinistra'],bins,label = 'Csx')
-plt.xlabel('Seats')
-plt.legend(loc='upper left')
-plt.savefig("Possible_outcome.png")
-plt.close()
-
-
-
-m = Montecarlo_electoral('txt')
-m.Import_Results()
-
-m.check_input()
-m.Complete_Simulation()
-m.Graphics()"""
